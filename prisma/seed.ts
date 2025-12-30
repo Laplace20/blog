50b3fa8a56094701f6e67bfd19d4226154bdbb73
Laplace20 <adinatadaffa28@gmail.com>
2025-12-30 23:43:27 +0800

    FixingDatabase

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 const bcrypt = require('bcryptjs')
 import 'dotenv/config'
+import { createUserWithProjects } from './projectSeed';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
@@ -21,7 +22,23 @@ async function main() {
     name: 'Saif Daffa Adinata', 
     password: hashedPassword,
     role: 'ADMIN',
+    project:{
+        create: [
+            {
+                title: 'Admin Project',
+                description: 'Description for Admin Project',
+                status: 'COMPLETED',
+                slug : 'admin-project',
+                link : 'https://example.com/admin',
+                github : 'AdminGitHub',
+                image : 'https://example.com/images/admin.png',
+                techStack:['Python', 'Django' ],
+            }
+        ]
+    } 
   },
+  
+  
 })
 
 console.log('Admin user created:', admin)
